c3e462e06d62e17cafd09d68f9e62df4aead4dd6
Crowdbotics <engineering@crowdbotics.com>
2023-05-14 23:18:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/crimson_cake_dev_83154/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crimson_cake_dev_83154;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "crimson_cake_dev_83154";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-cake-dev-83154",
+  url: "https://crimson-cake-dev-83154.botics.co",
+  api: "https://crimson-cake-dev-83154.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
